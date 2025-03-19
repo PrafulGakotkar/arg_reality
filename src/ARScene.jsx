1b6dac133fbf6ee4457c9c6507613c3dc2f1527a
PrafulGakotkar <prafulakotkar001@gmail.com>
2025-03-19 15:33:41 +0530

    first commit

--- src/ARScene.jsx
+++ src/ARScene.jsx
@@ -1,23 +1,35 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import 'aframe';
-import 'ar.js/aframe/build/aframe-ar'; // Import AR.js
+import 'ar.js/aframe/build/aframe-ar';
 
 function ARScene() {
-  navigator.xr.isSessionSupported('immersive-ar').then((supported) => {
-    if (supported) {
-        console.log('WebXR is supported');
-    } else {
-        console.log('WebXR is not supported');
+  useEffect(() => {
+    // Ensure A-Frame and AR.js are initialized after the DOM is ready
+    const scene = document.querySelector('a-scene');
+
+    if (scene && scene.hasLoaded) {
+      // Scene already loaded, do nothing
+      return;
     }
-});
+
+    if (scene) {
+      scene.addEventListener('loaded', () => {
+        console.log('A-Frame scene loaded.');
+      });
+    }
+
+    return () => {
+      // Cleanup if needed (e.g., remove event listeners)
+    };
+  }, []);
+
   return (
-    <a-scene embedded arjs="debugUIEnabled: false; trackingMethod: best;">
+    <a-scene
+      embedded
+      arjs="debugUIEnabled: false; trackingMethod: best; cameraParametersURL: './camera_para.dat'" // Ensure correct path
+    >
       <a-marker preset="hiro">
-        <a-entity
-          geometry="primitive: box; height: 0.5; width: 0.5; depth: 0.5;"
-          material="color: blue;"
-          position="0 0.5 0"
-        ></a-entity>
+        <a-box position="0 0.5 0" material="color: red;"></a-box>
       </a-marker>
       <a-entity camera></a-entity>
     </a-scene>
@@ -25,3 +37,35 @@ function ARScene() {
 }
 
 export default ARScene;
+
+
+
+
+// import React from 'react';
+// import 'aframe';
+// import 'ar.js/aframe/build/aframe-ar';
+
+// function ARScene() {
+//   navigator.xr.isSessionSupported('immersive-ar').then((supported) => {
+//     if (supported) {
+//         console.log('WebXR is supported');
+//     } else {
+//         console.log('WebXR is not supported');
+//     }
+// });
+//   return (
+    
+//     <a-scene embedded arjs="debugUIEnabled: false; trackingMethod: best; cameraParametersURL: 'path/to/camera_para.dat'">
+//         <a-marker preset="hiro">
+//         <a-entity
+//           geometry="primitive: box; height: 0.5; width: 0.5; depth: 0.5;"
+//           material="color: blue;"
+//           position="0 0.5 0"
+//         ></a-entity>
+//       </a-marker>
+//       <a-entity camera></a-entity>
+//     </a-scene>
+//   );
+// }
+
+// export default ARScene;
