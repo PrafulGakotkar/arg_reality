c316a90dca2836e2cb3be960d7acbc6f978cd611
PrafulGakotkar <prafulakotkar001@gmail.com>
2025-03-19 14:51:55 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import ARScene from './Components/ARScene'
+
+function App() {
+ 
+
+  return (
+    <>
+     <ARScene />
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/ARScene.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import 'aframe';
+import 'ar.js/aframe/build/aframe-ar'; // Import AR.js
+
+function ARScene() {
+  return (
+    <a-scene embedded arjs="debugUIEnabled: false; trackingMethod: best;">
+      <a-marker preset="hiro">
+        <a-entity
+          geometry="primitive: box; height: 0.5; width: 0.5; depth: 0.5;"
+          material="color: blue;"
+          position="0 0.5 0"
+        ></a-entity>
+      </a-marker>
+      <a-entity camera></a-entity>
+    </a-scene>
+  );
+}
+
+export default ARScene;
